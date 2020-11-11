86837418d3cf96ed13edf3ee5b0395e06695b91b
my name <myemailid@domain.com>
2020-11-11 10:18:19 +0100

    Google kalender implementering 11-11-2020

--- Components/CalendarComponent/CalendarView.js
+++ Components/CalendarComponent/CalendarView.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import {StyleSheet, Text, View, Alert} from 'react-native';
+import {StyleSheet, Text, View, Alert, TouchableOpacity, Image, Linking} from 'react-native';
 import CalendarPicker from 'react-native-calendar-picker';
 import HeaderNav from "../HeaderNav";
+import {Entypo} from "@expo/vector-icons";
+
 
 //Kalender klassen er en eksternt hentet komponent, som ikek er blevet tilpasset til porjektet endnu
 //Selve designet er under overvejelser, men er ikke fuldstændig fastlagt.
@@ -30,12 +32,21 @@ export default class CalendarView extends Component {
             selectedStartDate: date,
         });
     }
+    calendar = () => {
+        try {
+            Linking.openURL('https://calendar.google.com/calendar/u/0/r')
+        } catch (e) {
+            console.log(e.message);
+        }
+    };
+
     //I render instantieres en CalenderPicker komponent, der fremviser en kalender
     //Kalender har en property, som kan registrere valg af datoer, som forekommer ved tryk på skærmen
     render() {
         const { selectedStartDate } = this.state;
         const startDate = selectedStartDate ? selectedStartDate.toString() : '';
         return (
+            <View>
             <View style={{marginTop: 30}} >
                 <HeaderNav title="Kalender" />
                 <View style={{marginTop: 40}}>
@@ -44,9 +55,57 @@ export default class CalendarView extends Component {
                 <View>
                     <Text>SELECTED DATE:{ startDate }</Text>
                 </View>
+                </View>
+                <View>
+    <TouchableOpacity style={[styles.iconButtonsFacebook,]} onPress={this.calendar}>
+        <Text>Google kalender</Text>
+        <Image source={require('./assetsCalendar/GoogleCalendar.png')} style={{ width: 38, height: 38 }}/>
+    </TouchableOpacity>
+            </View>
             </View>
         );
     }
 }
 
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#DBF1EE',
+    },
+    iconButtonsFacebook: {
+        alignItems: 'center',
+        borderColor:'#47525E',
+        width: 140,
+        height: 84,
+        borderWidth:1,
+        borderRadius: 5,
+        bottom:65,
+        padding:10,
+        left:80,
+        marginTop: 200
+    },
+
+    buttonText:{
+        fontSize: 20,
+        color:'white',
+        fontWeight:'bold',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+
+    sigUpButton:{
+        alignItems: 'center',
+        width: 280,
+        height: 54,
+        padding: 10,
+        borderRadius: 5,
+        bottom:60,
+
+
+
+    }
+});
