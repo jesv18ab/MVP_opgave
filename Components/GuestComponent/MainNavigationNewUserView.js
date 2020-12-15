e048b526547e5c18c686d7e818e08a87f82441e1
my name <myemailid@domain.com>
2020-12-15 23:12:35 +0100

    MainNavigationNewUserView.js frontend 15-12-2020

--- Components/GuestComponent/MainNavigationNewUserView.js
+++ Components/GuestComponent/MainNavigationNewUserView.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { Button, View, StyleSheet } from 'react-native';
-
+import {Button, View, StyleSheet, Text, TouchableOpacity} from 'react-native';
+import { MaterialCommunityIcons, Entypo, Ionicons } from '@expo/vector-icons';
 
 
 export default class InitalViewNewUsers extends Component {
@@ -18,11 +18,23 @@ export default class InitalViewNewUsers extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <View style={styles.buttonContainer}>
-                    <Button title="Tryk her for at oprette et kollektiv" onPress={this.gotToCreate}/>
-                </View>
-                <View style={styles.buttonContainer}>
-                    <Button title="TRyk her for at se dine invitationer" onPress={this.gotToInvitations}/>
+                <Text style={styles.headerText}>Bliv en del af et  </Text>
+                <Text style={styles.headerText}>kollektiv  </Text>
+
+                <View style={{ flex: 1, flexDirection: 'row', top: '5%'}}>
+                    <TouchableOpacity style={{width: '40%', height: '20%', backgroundColor: '#47525E',   borderRadius: 10,
+                        overflow: 'hidden', right: 10, top: 80, alignItems: 'center', justifyContent: 'center' }} onPress={this.gotToCreate}>
+                        <Entypo name="home" size={50} color="white" />
+
+                        <Text style={{color: 'white', fontSize: 18}}>Opret kollektiv</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={{width: '40%', height: '20%', backgroundColor: '#47525E',   borderRadius: 10,
+                        overflow: 'hidden', left: 10,  top: 80, alignItems: 'center', justifyContent: 'center' }} onPress={this.gotToInvitations}>
+                        <MaterialCommunityIcons name="email-newsletter" size={50} color="white" />
+                        <Text style={{color: 'white', fontSize: 18}}>Se invitationer</Text>
+
+                    </TouchableOpacity>
+
                 </View>
             </View>
         );
@@ -31,11 +43,20 @@ export default class InitalViewNewUsers extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        flexDirection: 'row',
-
+        alignItems: 'center',
         justifyContent: 'center',
+        backgroundColor: '#DBF1EE',
+        width: '100%',
+        height: '100%',
     },
     buttonContainer: {
-        flex: 1,
-    }
+        height: '10%',
+        width: 10,
+        backgroundColor: '#767577'
+    },
+    headerText: {
+        fontSize: 50,
+        fontWeight:'bold',
+        color:'#5FB8B2',
+    },
 });
